fb0bae2f067e0eeef4c316326336f8152dc8cb5b
Magnus Wållberg <magnus@tagpad.net>
2025-12-07 00:35:02 +0100

    Add truncate, countOccurrences, and repeat methods to StringUtils

--- src/main/java/net/tagpad/packageA/StringUtils.java
+++ src/main/java/net/tagpad/packageA/StringUtils.java
@@ -18,4 +18,35 @@ public class StringUtils {
         }
         return new StringBuilder(str).reverse().toString();
     }
+
+    public static String truncate(String str, int maxLength) {
+        if (str == null || str.length() <= maxLength) {
+            return str;
+        }
+        return str.substring(0, maxLength) + "...";
+    }
+
+    public static int countOccurrences(String str, char ch) {
+        if (str == null) {
+            return 0;
+        }
+        int count = 0;
+        for (char c : str.toCharArray()) {
+            if (c == ch) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static String repeat(String str, int times) {
+        if (str == null || times <= 0) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < times; i++) {
+            sb.append(str);
+        }
+        return sb.toString();
+    }
 }
