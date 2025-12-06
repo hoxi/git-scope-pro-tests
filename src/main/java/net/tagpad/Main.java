c90e33567addf92e98f6e7d7d53540b1f56dbae1
Magnus Wållberg <magnus@tagpad.net>
2025-12-06 23:15:38 +0100

    Remove excessive debug logging from Main.java

--- src/main/java/net/tagpad/Main.java
+++ src/main/java/net/tagpad/Main.java
@@ -9,18 +9,15 @@ public class Main {
         LoggingService logger = new LoggingService();
 
         logger.logInfo("Application started");
-        logger.logDebug("Initializing main method");
 
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-        logger.logInfo("Welcome message displayed");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
-            logger.logDebug("Loop iteration: " + i);
         }
 
         logger.logInfo("Application finished successfully");
