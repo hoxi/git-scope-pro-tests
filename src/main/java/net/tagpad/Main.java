6ee9035db5c2bace3701b9256b1b09def6a9a262
Magnus Wållberg <magnus@tagpad.net>
2025-12-06 22:45:16 +0100

    Add logging statements to Main.java using LoggingService

--- src/main/java/net/tagpad/Main.java
+++ src/main/java/net/tagpad/Main.java
@@ -1,17 +1,28 @@
 package net.tagpad;
 
+import net.tagpad.packageB.LoggingService;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        LoggingService logger = new LoggingService();
+
+        logger.logInfo("Application started");
+        logger.logDebug("Initializing main method");
+
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        logger.logInfo("Welcome message displayed");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+            logger.logDebug("Loop iteration: " + i);
         }
+
+        logger.logInfo("Application finished successfully");
     }
 }
